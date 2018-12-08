99f3e33f3801ee20da3ed1bf8dd6b3cab1e5b46b
afcarioca <afcarioca@hotmail.com>
2018-12-08 11:23:26 -0200

    Apliando o código da Funcao logica_do_jogo

--- programa-forca/logica.rb
+++ programa-forca/logica.rb
@@ -1,6 +1,31 @@
 require_relative "ui"
 
+def cria_mascara chutes, palavra_secreta
+	mascara = "_"
+	
+	for letra in palavra_secreta.chars
+		if chutes.include? letra 
+			mascara += letra
+		else
+			mascara += "_"
+		end	
+	end	
 
+	mascara
+end	
+
+def verifica_chute_repetido chute, chutes
+
+	loop do
+		if chutes.include? chute
+			repetido
+			chute = pede_o_chute
+		else
+			break
+		end	
+	end
+
+end	
 
 def logica_do_jogo
 
@@ -16,24 +41,37 @@ def logica_do_jogo
 
 	while erros < 5
 
+			mascara = cria_mascara chutes palavra_secreta
+
+			mostra_mascara mascara
+
 			chute = pede_o_chute
+
+			verifica_chute_repetido chute, chutes
 
 			chutes << chute
 
 			if chute.size > 1
 				if chute == palavra_secreta
+					ganhou_o_jogo
 					pontos_acumulados += 100
 					break
 				else
-					erros+=1
+					erros +=1
 					pontos_acumulados -= 30
 					next	
 				end
 			else
-				mostra_mascara chutes, palavra_secreta
+				if palavra_secreta.chars.include? chute
+					acertou palavra_secreta.count(chute)	
+					next		
+				else
+					erros += 1
+					mostra_erros erros
+				end	
 			end		
 	end	
 
-	
+	quantidade_de_pontos pontos_acumulados
 
 end	
