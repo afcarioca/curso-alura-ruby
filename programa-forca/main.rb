eae91e1a089f4df479ee6ca9470679a2a517590a
afcarioca <afcarioca@hotmail.com>
2018-12-08 10:06:46 -0200

    Teste da Função pede_o_chute OK

--- programa-forca/main.rb
+++ programa-forca/main.rb
@@ -1,7 +1,11 @@
 require_relative "ui"
 
-apresenta_jogo
+#apresenta_jogo
 
-palavra_secreta = sorteia_palavra_secreta
+#palavra_secreta = sorteia_palavra_secreta
 
-puts "#{palavra_secreta}"
+#puts "#{palavra_secreta}"
+
+chute = pede_o_chute
+
+puts "#{chute}"
